853ce74e2a30e0230eaccf904da0ae5d88a800a1
radicalmove <richard.davies@canterbury.ac.nz>
2026-03-09 12:17:31 +1300

    Lighten RADcast top-end processing

--- src/radcast/constants.py
+++ src/radcast/constants.py
@@ -10,11 +10,10 @@ DEFAULT_ENHANCE_LAMBD = 0.7
 DEFAULT_ENHANCE_TAU = 0.5
 DEFAULT_ENHANCE_POSTFILTER = (
     "highpass=f=60,"
-    "equalizer=f=135:t=q:w=1.15:g=4.2,"
-    "equalizer=f=250:t=q:w=1.0:g=2.6,"
-    "equalizer=f=3000:t=q:w=1.0:g=0.8,"
-    "deesser=i=0.10:m=0.35:f=0.5:s=o,"
-    "acompressor=threshold=-18dB:ratio=1.7:attack=25:release=140:makeup=1.6,"
+    "equalizer=f=135:t=q:w=1.15:g=4.0,"
+    "equalizer=f=245:t=q:w=1.0:g=2.4,"
+    "equalizer=f=3000:t=q:w=1.0:g=0.9,"
+    "acompressor=threshold=-20dB:ratio=1.3:attack=35:release=120:makeup=1.0,"
     "loudnorm=I=-18:TP=-1.5:LRA=7"
 )
 DEFAULT_WORKER_FALLBACK_TIMEOUT_SECONDS = 40
